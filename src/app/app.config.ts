afc62198ff7a466243399ed035489c977ffa6a65
arpho <damicogiuseppe77@gmail.com>
2025-01-07 17:30:17 +0100

    installing firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,29 @@
+import { environment } from './../environments/environment.prod';
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { FIREBASE_OPTIONS } from '@angular/fire/compat';
 import { routes } from './app.routes';
-
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
+import { getFunctions, provideFunctions } from '@angular/fire/functions';
+import { getStorage, provideStorage } from '@angular/fire/storage';
+import { provideClientHydration } from '@angular/platform-browser';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideDatabase(() => getDatabase()),
+    provideFunctions(() => getFunctions()),
+    provideStorage(() => getStorage()),
+    { provide: FIREBASE_OPTIONS, useValue: environment.firebaseConfig },
+    provideClientHydration(),
+  ],
 };
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,19 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { enableProdMode } from '@angular/core';
+import { environment } from '../src/environments/environment.prod';
+
+if (environment.production) {
+  // Override console methods to suppress logs/*
+  /*console.log = () => {};
+  console.debug = () => {};
+  console.warn = () => {}; */
+  // console.error = () => {}; // Optional, depending on requirements
+}
+if (environment.production) {
+  enableProdMode();
+}
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
