df42b8cd35b6eddd92558e6a4494567551e26d6e
arpho <damicogiuseppe77@gmail.com>
2025-01-20 17:42:07 +0100

    started implementing signup component wip

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,13 +4,13 @@ import { NgModule } from '@angular/core';
 import { authGuard } from './guards/auth.guard';
 import { LoginComponent } from './components/login/login.component';
 import { TestComponent } from './components/test/test.component';
-import { SigninComponent } from './components/signin/signin.component';
+import { SignupComponent } from './components/signin/signin.component';
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent, canActivate: [authGuard] },
   { path: 'login', component: LoginComponent },
   { path: 'test', component: TestComponent },
-  {path: 'signup', component: SigninComponent},
+  {path: 'signup', component: SignupComponent},
   { path: '', redirectTo: '/home', pathMatch: 'full' },
 ];
 
--- src/app/components/signin/signin.component.ts
+++ src/app/components/signin/signin.component.ts
@@ -1,12 +1,46 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { UserModel } from '../../models/userModel';
 
 @Component({
-  selector: 'app-signin',
-  imports: [],
+  selector: 'app-signup',
+  imports: [    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatIconModule,
+    MatCardModule,
+    MatDividerModule,
+    MatButtonModule,
+    CommonModule,
+    FormsModule,],
   templateUrl: './signin.component.html',
   styleUrl: './signin.component.css',
   standalone: true
 })
-export class SigninComponent {
+export class SignupComponent {
+submit() {
+const user= new UserModel({
+  email: this.email,
+  password: this.password,
+  firstName: this.firstName,
+  lastName: this.lastName
+})
+console.log("user",user)
+}
+
+  email: string = '';
+  password: string = '';
+  error: boolean = false;
+  errorMessage: string = '';
+  firstName: string = '';
+  lastName: string = '';
 
 }
