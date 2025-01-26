b5285204720651fc2dd9f0cf373a433113c22f9b
arpho <damicogiuseppe77@gmail.com>
2025-01-26 18:10:11 +0100

    removed unused import

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import { LoginComponent } from "./components/login/login.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LoginComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
