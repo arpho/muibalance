709d9f564212171c4aad3cff0f74e501ed590214
arpho <damicogiuseppe77@gmail.com>
2025-05-12 12:18:22 +0200

    material gridList
    #TODO fix visualization

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { SectionCardComponent } from "../section-card/section-card.component";
+import {MatGridListModule} from '@angular/material/grid-list';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
-  imports: [SectionCardComponent]
+  imports: [
+    SectionCardComponent,
+    MatGridListModule
+  ]
 })
 export class HomeComponent implements OnInit {
 
