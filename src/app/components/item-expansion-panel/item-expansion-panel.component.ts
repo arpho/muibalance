f0b8f5b868e552186ae0c576b7941b0cd6f1df99
arpho <damicogiuseppe77@gmail.com>
2025-05-24 19:47:20 +0200

    Adds item expansion panel for editing
    Adds an item expansion panel component to allow users to edit item details.
    
    Implements menu options for updating and deleting items from the table.
    Enhances the table row selection to pass item data to the expansion panel.
    at the momnt the delete function is just a place holder

--- src/app/components/item-expansion-panel/item-expansion-panel.component.ts
+++ src/app/components/item-expansion-panel/item-expansion-panel.component.ts
@@ -1,19 +1,72 @@
-import { Component, input, model, OnChanges, signal, SimpleChanges } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input, model, OnChanges, output, signal, SimpleChanges } from '@angular/core';
 import { ItemsModel } from '../../models/itemsModel';
+import { ReactiveFormsModule, FormsModule, FormGroup, Form, FormBuilder } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatFormField, MatFormFieldModule, MatLabel } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-item-expansion-panel',
-  imports: [],
+  imports: [MatExpansionModule,
+        ReactiveFormsModule,
+        FormsModule,
+        MatFormField,
+        MatFormFieldModule,
+        MatInputModule,
+        MatButtonModule,
+        MatLabel,],
   templateUrl: './item-expansion-panel.component.html',
   styleUrl: './item-expansion-panel.component.css',
-  standalone: true
+  standalone: true,
+  changeDetection:ChangeDetectionStrategy.OnPush
 })
 export class ItemExpansionPanelComponent implements OnChanges{
-  data=input<{item:ItemsModel,index?:number,operation?:string}>( {item:new ItemsModel(),index:0,operation:""} )
 
-panelIsOpen=signal
+  data=input<{item:ItemsModel,operation:string,index?:number }|null>()
+  updated=output<{item:ItemsModel,operation:string,index?:number}>()
+  descrizione=signal("")
+  note=signal("")
+  prezzo=signal(0)
+  moneta=signal("")
+  picture=signal("")
+  categoriesKey=signal<string[]>([])
+
+panelIsOpen=signal(false)
+itemForm : FormGroup = new FormGroup({})
+immagine: string = '';
   ngOnChanges(changes: SimpleChanges): void {
-    throw new Error('Method not implemented.');
+    if(changes["data"].currentValue){
+      console.log("data",changes["data"].currentValue)
+      this.descrizione.set(changes["data"].currentValue.item.descrizione)
+      this.note.set(changes["data"].currentValue.item.note)
+      this.prezzo.set(changes["data"].currentValue.item.prezzo)
+      this.moneta.set(changes["data"].currentValue.item.moneta)
+      this.picture.set(changes["data"].currentValue.item.picture)
+      this.categoriesKey.set(changes["data"].currentValue.item.categoriesKey)
+      this.panelIsOpen.set(true)
+      this.itemForm = this.fb.group({
+        descrizione: this.descrizione(),
+        note: this.note(),
+        prezzo: this.prezzo(),
+        moneta: this.moneta(),
+        picture: this.picture(),
+        categoriesKey: this.categoriesKey()
+      })
+
+    }
+  }
+  constructor(
+    private fb:FormBuilder
+  ) {
+    this.itemForm = this.fb.group({
+      descrizione: this.descrizione(),
+      note: this.note(),
+      prezzo: this.prezzo(),
+      moneta: this.moneta(),
+      picture: this.picture(),
+      categoriesKey: this.categoriesKey()
+    })
   }
 
 }
--- src/app/components/items-table/items-table.component.ts
+++ src/app/components/items-table/items-table.component.ts
@@ -1,10 +1,12 @@
-import { Component,  model, OnInit, output } from '@angular/core';
+import { Component,  model, OnInit, output, signal } from '@angular/core';
 import { ItemsModel } from '../../models/itemsModel';
 import { MatTableModule } from '@angular/material/table';
 import { CategoryViewerComponent } from '../category-viewer/category-viewer.component';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { ItemExpansionPanelComponent } from '../item-expansion-panel/item-expansion-panel.component';
+import { MatMenu, MatMenuModule } from '@angular/material/menu';
 
 @Component({
   selector: 'app-items-table',
@@ -13,15 +15,36 @@ import { MatButtonModule } from '@angular/material/button';
     CategoryViewerComponent,
     MatDividerModule,
     MatIconModule,
-    MatButtonModule
+    MatButtonModule,
+    ItemExpansionPanelComponent,
+    MatMenuModule
   ],
   templateUrl: './items-table.component.html',
   styleUrl: './items-table.component.css',
   standalone: true
 })
 export class ItemsTableComponent implements  OnInit{
-insertNewItem() {
+selectItem(item: any, index: any) {
+console.log("selectItem from item selector", item,index)
+this.selectedItem.set(item)
+this.selectedIndex.set(index)
+}
+  selectedIndex=signal<number|null>(null)
+  selectedItem=signal<ItemsModel|null>(null)
+deleteItem($event: MouseEvent) {
 throw new Error('Method not implemented.');
+}
+updateItem($event: any) {
+  console.log("updateItem from item selector", this.selectedItem(),"on index",this.selectedIndex())
+this.item2edit.set({item:this.selectedItem()!,operation:"update",index:this.selectedIndex()!})
+}
+Item2beEdited($event: any) {
+this.item2edit.set({item:this.selectedItem()!,operation:"update",index:this.selectedIndex()!})
+}
+item2edit=signal<{item:ItemsModel,operation:string,index?:number}|null>(null)
+insertNewItem() {
+console.log("insertNewItem")
+this.item2edit.set({item:new ItemsModel(),operation:"add"})
 }
     displayedColumns: string[] = ['descrizione',  'note','prezzo','categorie'];
   ngOnInit(): void {
