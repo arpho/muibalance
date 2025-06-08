97827f26397d3e1a43f9fad88d9a1ca9387b634d
arpho <damicogiuseppe77@gmail.com>
2025-06-08 14:55:15 +0200

    confirm dialog wip

--- src/app/components/confirm-dialog/confirm-dialog.component.ts
+++ src/app/components/confirm-dialog/confirm-dialog.component.ts
@@ -1,13 +1,44 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import {
+  MAT_DIALOG_DATA,
+  MatDialog,
+  MatDialogActions,
+  MatDialogClose,
+  MatDialogContent,
+  MatDialogRef,
+  MatDialogTitle,
+} from '@angular/material/dialog';
 
+export interface ConfirmDialogData {
+  title: string;
+  message: string;
+}
 @Component({
   selector: 'app-confirm-dialog',
-  imports: [],
+  imports: [
+
+    MatButtonModule,
+    MatDialogTitle,
+    MatDialogContent,
+    MatDialogActions,
+    MatDialogClose,
+  ],
   templateUrl: './confirm-dialog.component.html',
   styleUrl: './confirm-dialog.component.css',
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ConfirmDialogComponent {
+  constructor(
+    private dialogRef: MatDialogRef<ConfirmDialogComponent>
+  )
+  {}
 
+ readonly data = inject<ConfirmDialogData>(MAT_DIALOG_DATA);
+onNoClick() {
+this.dialogRef.close({confirmation:false})
+
+
+}
 }
